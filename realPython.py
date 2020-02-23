b3721eae0bcf05db721f8afa196251748bb735dc
PassionateLooker <dayasalian27@gmail.com>
2020-02-23 15:59:22 +0530

    new update

--- realPython.py
+++ realPython.py
@@ -23,12 +23,23 @@
 # a={'4':'3','2':'1','5':'3','0':'1'}
 # r=sorted(a.items(),key=lambda x:x[0])
 # print(r)
-a,b,c=1,1,1
-if a==1 or b==1 or c==1:
-    print('true')
-if a or b or c:
-    print('t')
-if any((a,b,c)):
-    print('tr')
-if
+# a,b,c=1,1,1
+# if a==1 or b==1 or c==1:
+#     print('true')
+# if a or b or c:
+#     print('t')
+# if any((a,b,c)):
+#     print('tr')
+# if 1 in (a,b,c):
+#     print('tru')
 
+# name_for_userid = {
+#     382: "Alice",
+#     590: "Bob",
+#     951: "Dilbert",
+# }
+#
+# def nameGetter(name):
+#     return 'hi %s!'%name_for_userid.get(name,'there')
+#
+# print(nameGetter(5920))
